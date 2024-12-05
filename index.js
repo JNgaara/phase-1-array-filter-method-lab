4d19f4383a85da81d42d2f19e7abed80a6ada2bb
Joshua Ngaara <joshua.ngaara@student.moringaschool.com>
2024-12-05 08:18:23 +0200

    commit e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+let acc= [];
+let matches=drivers.filter(findMatching =>);
+let findMatching= drivers.filter(drNames, str){
+    
+//     for(let i=0;i<drNames.length;i++) {
+//         if(drNames[i]===str){
+//             acc.push(drNames[i]);
+// return drNames[i];
+//         }
+//     }
+    //return acc;
+}
+
+//result= findMatching(drivers,'Sammy')
+console.log(result);
+
+function fuzzyMatch(drNames, str){
+    
+}
+
+function matchName(drNames, str){
+
+}
